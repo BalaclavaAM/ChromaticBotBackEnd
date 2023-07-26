5ebca3ab17367b4a9f4c13f41ed1b5bcf2935e93
Juan Camacho <68751794+BalaclavaAM@users.noreply.github.com>
2023-07-25 23:09:26 -0500

    feature: Now added the colorname

--- chromaticLogic.py
+++ chromaticLogic.py
@@ -1,5 +1,3 @@
-import math
-import cv2
 import numpy as np
 from sklearn.cluster import KMeans
 import urllib.request as urllib2
@@ -9,7 +7,7 @@ import colorthief
 def extract_color_palette_and_dominant(image_path):
     color_thief = colorthief.ColorThief(image_path)
     palette = color_thief.get_palette(color_count=6)
-    dominant = color_thief.get_color(quality=5)
+    dominant = color_thief.get_color(quality=7)
     return (palette, dominant)
 
 def histogram_similarity(hist1, hist2):
@@ -45,31 +43,97 @@ def group_images_by_histogram_similarity(images_info, threshold):
 
     return groups
 
-def retrieve_chromatic_order_from_spotify_data(spotify_data: dict)->list:
-    new_items=[]
-    diccionario_temporal={}
+from colorsys import rgb_to_hsv
+
+def retrieve_hue(rgb):
+    r, g, b = rgb
+    h, _, _ = rgb_to_hsv(r / 255.0, g / 255.0, b / 255.0)
+    return h
+
+def classify_color(rgb)->str:
+    # The uidea is classify based in the 8 intervals of the hue
+    # and retrieve the color name
+    # 0-45 red
+    # 45-90 orange
+    # 90-135 yellow
+    # 135-180 green
+    # 180-225 cyan
+    # 225-270 blue
+    # 270-315 magenta
+    # 315-360 red
+    #check if the color is black or white
+    hsv = rgb_to_hsv(rgb[0] / 255.0, rgb[1] / 255.0, rgb[2] / 255.0)
+    hue = hsv[0] * 360
+
+    if hsv[2] < 0.1:  # If value is very low, it's black
+        return "black"
+    elif hsv[1] < 0.1:  # If saturation is very low, it's white/gray
+        return "white/gray"
+    elif 355 <= hue < 10:  # Red
+        return "red"
+    elif 11 <= hue < 20:  # Red-Orange
+        return "red-orange"
+    elif 21 <= hue < 40:  # Orange & Brown
+        return "orange/brown"
+    elif 41 <= hue < 50:  # Orange-Yellow
+        return "orange-yellow"
+    elif 51 <= hue < 60:  # Yellow
+        return "yellow"
+    elif 61 <= hue < 80:  # Yellow-Green
+        return "yellow-green"
+    elif 81 <= hue < 140:  # Green
+        return "green"
+    elif 141 <= hue < 169:  # Green-Cyan
+        return "green-cyan"
+    elif 170 <= hue < 200:  # Cyan
+        return "cyan"
+    elif 201 <= hue < 220:  # Cyan-Blue
+        return "cyan-blue"
+    elif 221 <= hue < 240:  # Blue
+        return "blue"
+    elif 241 <= hue < 280:  # Blue-Magenta
+        return "blue-magenta"
+    elif 281 <= hue < 320:  # Magenta
+        return "magenta"
+    elif 321 <= hue < 330:  # Magenta-Pink
+        return "magenta-pink"
+    else:
+        return "unknown"
+
+def retrieve_chromatic_order_from_spotify_data(spotify_data: dict) -> list:
+    new_items = []
+    diccionario_temporal = {}
     for item in spotify_data["items"]:
         position = -1
-        #buscar posicion del album en el diccionario
+        # buscar posicion del album en el diccionario
         if item["album"]["id"] in diccionario_temporal:
             position = diccionario_temporal[item["album"]["id"]]
         else:
-            diccionario_temporal[item["album"]["id"]] = len(diccionario_temporal)
-            position = diccionario_temporal[item["album"]["id"]]
-        if position != -1:
+            diccionario_temporal[item["album"]["id"]] = len(new_items)
+
+        if position == -1:
             new_item = {}
             image_url = item["album"]["images"][0]["url"]
             image_path = "./imageCache/" + item["id"] + ".jpg"
-            if (not os.path.isfile(image_path)):
+            if not os.path.isfile(image_path):
                 urllib2.urlretrieve(image_url, image_path)
             color_palette_dominant = extract_color_palette_and_dominant(image_path)
             new_item["album"] = item["album"]["name"]
             new_item["image"] = item["album"]["images"][0]["url"]
             new_item["colors"] = color_palette_dominant[0]
             new_item["dominant"] = color_palette_dominant[1]
+            new_item["color_names"] = [classify_color(color) for color in color_palette_dominant[0]]
+            new_item["colorfulness"] = retrieve_hue(color_palette_dominant[1])
             new_items.append(new_item)
-        new_item=new_items[position]
-        lista_canciones = new_item.get("songs", [])
-        lista_canciones.append({"name": item["name"], "artists": ", ".join([artist["name"] for artist in item["artists"]])})
-        new_item["songs"] = lista_canciones
-    return group_images_by_histogram_similarity(new_items, 0.7)
+            position = diccionario_temporal[item["album"]["id"]]
+            new_item = new_items[position]
+            lista_canciones = new_item.get("songs", [])
+            lista_canciones.append({"name": item["name"], "artists": ", ".join([artist["name"] for artist in item["artists"]])})
+            new_item["songs"] = lista_canciones
+
+    # Sort new_items based on the chromatic order (colorfulness of dominant color)
+    new_items.sort(key=lambda x: x["colorfulness"])
+
+
+    return new_items
+
--- spotifyApiConsumer.py
+++ spotifyApiConsumer.py
@@ -5,7 +5,7 @@ def get_top_50(access_token: str)->dict:
         "Authorization": f"Bearer {access_token}"
     }
     #get top 50 songs
-    top_50_songs = requests.get("https://api.spotify.com/v1/me/top/tracks?limit=15", headers=headers).json()
+    top_50_songs = requests.get("https://api.spotify.com/v1/me/top/tracks?limit=50", headers=headers).json()
     if "error" in top_50_songs:
         raise Exception("Invalid access token")
     return top_50_songs
